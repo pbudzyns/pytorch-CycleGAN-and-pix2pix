c16d1b8fe651d1c28a25c1ae5443ac225054ca18
Pawel Budzynski <pawel.budzynski19+git@gmail.com>
2023-03-24 12:47:32 +0100

    add benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,13 +1,15 @@
+import argparse
 import sys
+from typing import List, Tuple
+
 import torch.utils.benchmark as benchmark
 
-from data import create_dataset
-from models import create_model
+from data import BaseDataset, create_dataset
+from models import BaseModel, create_model
 from options.train_options import TrainOptions
 
 
-def run_test_train_loop(dataset, model, opt):
-
+def run_test_train_loop(dataset: BaseDataset, model: BaseModel, opt: argparse.Namespace) -> None:
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):
         model.update_learning_rate()
         for i, data in enumerate(dataset):
@@ -15,51 +17,65 @@ def run_test_train_loop(dataset, model, opt):
             model.optimize_parameters()
 
 
-def generate_datasets(args):
-    sys.argv.extend(args)
-    opt = TrainOptions().parse()
-    dataset = create_dataset(opt)
-    model = create_model(opt)
-    model.setup(opt)
-    return model, dataset, opt
+def get_options(args: List[str]) -> argparse.Namespace:
+    sys.argv.extend(args)  # fishy but works
+    return TrainOptions().parse()
 
 
-def get_args(compiled: bool, batch_size: int = 1):
-    args = [
+def generate_models_and_dataset(options: argparse.Namespace) -> Tuple[BaseModel, BaseDataset]:
+    dataset = create_dataset(options)
+    model = create_model(options)
+    model.setup(options)
+    return model, dataset
+
+
+def get_args(
+        batch_size: int = 1,
+        n_epochs: int = 10,
+        generator_network: str = 'animegan',
+        model_name: str = 'cycle_gan_lpips',
+) -> List[str]:
+    return [
         '--dataroot', './datasets/benchmark',
         '--name', 'Benchmark',
-        '--netG', 'animegan',
+        '--netG', generator_network,
         '--batch_size', str(batch_size),
-        '--n_epochs', '10',
-        '--n_epochs_decay', '0',
-        '--model', 'cycle_gan_lpips'
+        '--n_epochs', str(n_epochs),
+        '--n_epochs_decay', str(n_epochs),
+        '--model', model_name
     ]
-    if compiled:
-        args.append('--compile')
-    return args
+
+
+def benchmark_single_model(
+        options: argparse.Namespace, description: str, threads_range: Tuple[int, ...] = (1, 4, 8, 16),
+) -> List[benchmark.Measurement]:
+
+    model, dataset = generate_models_and_dataset(options)
+    results = []
+    for num_threads in threads_range:
+        results.append(
+            benchmark.Timer(
+                stmt='run_test_train_loop(dataset, model, opt)',
+                setup='from __main__ import run_test_train_loop',
+                globals={'dataset': dataset, 'model': model, 'opt': options},
+                num_threads=num_threads,
+                label='CycleGAN training',
+                description=description,
+            ).blocked_autorange(min_run_time=1)
+        )
+    return results
 
 
 if __name__ == '__main__':
-    results = []
-    label = 'CycleGAN training'
 
-    for num_threads in [1, 4, 8, 16]:
-        results.append(benchmark.Timer(
-            stmt='run_test_train_loop(args)',
-            setup='from __main__ import run_test_train_loop',
-            globals={'args': get_args(compiled=False)},
-            num_threads=num_threads,
-            label=label,
-            description='Normal',
-        ).blocked_autorange(min_run_time=1))
-        results.append(benchmark.Timer(
-            stmt='run_test_train_loop(args)',
-            setup='from __main__ import run_test_train_loop',
-            globals={'args': get_args(compiled=True)},
-            num_threads=num_threads,
-            label=label,
-            description='Compiled',
-        ).blocked_autorange(min_run_time=1))
+    options = get_options(get_args(batch_size=1))
 
+    options.compile = False
+    results_normal = benchmark_single_model(options, description='Normal', threads_range=(1, 2))
+
+    options.compile = True
+    results_compile = benchmark_single_model(options, description='Compiled', threads_range=(1, 2))
+
+    results = results_normal + results_compile
     compare = benchmark.Compare(results)
     compare.print()
